4cbc0c21c8d5d7528ffe08d741e53653c0f6d7e2
hebatist <hebaja@yahoo.com.br>
2025-11-05 21:47:12 -0300

    ex02 done

---
+++ ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int	main()
+{
+	std::string	msg = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &msg;
+	std::string &stringREF = msg;
+
+	std::cout << &msg << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << msg << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+
+	return (0);
+}
