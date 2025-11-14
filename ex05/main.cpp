4b08bd5d94af6ff68d0a419f4356379fc5fd0b9f
Henrique <hebaja@yahoo.com.br>
2025-11-14 20:32:06 -0300

    ex05 improving main.cpp

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -7,14 +7,18 @@ int	main()
 
 	void (Harl::*ptrComplain)(std::string) = &Harl::complain;
 
-	std::cout << "DEBUG: ";
+	std::cout << "[ DEBUG ]" << std::endl;
 	(harl.*ptrComplain)("debug");
-	std::cout << "INFO: ";
+	std::cout << std::endl;
+	std::cout << "[ INFO ]" << std::endl;
 	(harl.*ptrComplain)("info");
-	std::cout << "WARNING: ";
+	std::cout << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
 	(harl.*ptrComplain)("warning");
-	std::cout << "ERROR: ";
+	std::cout << std::endl;
+	std::cout << "[ ERROR ]" << std::endl;
 	(harl.*ptrComplain)("error");
+	std::cout << std::endl;
 	
 	(harl.*ptrComplain)("nonexistent");
 	(harl.*ptrComplain)("");
